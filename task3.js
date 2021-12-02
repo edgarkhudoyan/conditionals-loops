5a4638ffe6681d5fbd9fb3e4a9cb4fa64c33dd97
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 13:26:40 +0400

    Added toArr method

--- task3.js
+++ task3.js
@@ -1,9 +1,9 @@
 // Given a positive integer. Bring the last digit of the number to the beginning. Print the new number. If the last digit of the inserted number is 0, number remains the same.
 'use strict';
-let num = 350;
+let num = 750;
 
-if (num.toString().split('')[-1] === 0) {
+let toArr = num.toString().split('');
+
+if (toArr[toArr.length - 1] === '0') {
   console.log(num);
-} else {
-  console.log(num.toString().split('').reverse().join(''));
 }
