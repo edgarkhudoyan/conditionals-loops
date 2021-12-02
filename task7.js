c9dd39c5132ba9dd7dd28864a36fa44c5c76e9ed
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 14:42:28 +0400

    Tried string methods

--- task7.js
+++ task7.js
@@ -1,1 +1,7 @@
 // Insert a digit and a number. Check whether the digits contains in the number or not.
+let digit = parseInt(prompt('Please insert a digit'));
+let number = parseInt(prompt('Please insert a number'));
+
+let str = digit.toString.split('');
+
+console.log(str.includes(number));
