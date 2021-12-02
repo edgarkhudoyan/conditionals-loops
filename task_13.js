0260b9987f143d5498330e3fa0494035b812e449
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 23:42:01 +0400

    Filtered the array

--- task_13.js
+++ task_13.js
@@ -1,2 +1,7 @@
 // Given mixed array of numbers, strings, booleans, nulls and undefined. Filter array and get all the numbers in a separate array. Arrange them such as from the beginning are the odds and from the ending the evens.
 'use strict';
+let mixedArr = [8, 0, 1, 'hey', 12, 5, true, '2', null, 7, 3];
+
+let filteredArr = mixedArr.filter((item) => {
+  return typeof item === 'number';
+});
