57845038590253c06c6fce6af31ac135967389f6
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 23:33:47 +0400

    Solved task_12

--- task_12.js
+++ task_12.js
@@ -1,1 +1,13 @@
 // Given a sentence with missing words and an array of words. Replace all ‘_ʼ in a sentence with the words from the array.
+'use strict';
+let arr1 = ['Houston', 'problem'];
+let arr2 = ['first', 'succeed', 'again'];
+let arr3 = ['Force', 'be', 'with', 'you'];
+
+let sentance1 = console.log(`${arr1[0]} we have a ${arr1[1]}`);
+let sentance2 = console.log(
+  `If at ${arr2[0]} you don't ${arr2[1]}, try, try ${arr2[2]}`
+);
+let sentance3 = console.log(
+  `May the ${arr3[0]} ${arr3[1]} ${arr3[2]} ${arr3[3]}`
+);
