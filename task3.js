7c7645f673d18be856b2596d1184a9754fbcd32e
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 13:10:59 +0400

    Use if statement

--- task3.js
+++ task3.js
@@ -1,1 +1,9 @@
 // Given a positive integer. Bring the last digit of the number to the beginning. Print the new number. If the last digit of the inserted number is 0, number remains the same.
+'use strict';
+let num = 350;
+
+if (num.toString().split('')[-1] === 0) {
+  console.log(num);
+} else {
+  console.log(num.toString().split('').reverse().join(''));
+}
