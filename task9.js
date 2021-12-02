63f07617e76cd369591da21f8022f1d931782067
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 22:50:42 +0400

    Tried if statement

--- task9.js
+++ task9.js
@@ -1,2 +1,9 @@
 // Insert a number. Print ‘yesʼ if the number is prime, ‘noʼ otherwise.
 'use strict';
+let num = 14;
+
+if (num % num === 0 && num % 1 === 0) {
+  console.log('Yes');
+} else if (num % num === 1) {
+  console.log('No');
+}
