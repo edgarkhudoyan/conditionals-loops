b3fc484fbb204ec92225162963174e44cff43a5c
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 12:47:16 +0400

    Task 1 is done

--- task1.js
+++ task1.js
@@ -1,11 +1,9 @@
 // Given a number. Print “odd” if the number is odd and “even” if itʼs even.
 
-let num = 20;
+let num = 294;
 
-if (num % 2 === 0) {
+if (num % 2 == 0) {
   console.log('even');
-} else if (num % 2 === 1) {
-  console.log('odd');
 } else {
-  console.log('Please write a valid number');
+  console.log('odd');
 }
