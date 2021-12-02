483fd3be07f7c6cfedf9579faa8d863d9f0623e4
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 23:14:34 +0400

    Added function fibonacci

--- task10.js
+++ task10.js
@@ -1,2 +1,11 @@
 // Given a number n ( n >= 0 ) . Print n Fibonacci number. (Fibonacci th series: 0, 1, 1, 2, 3, 5, 8 ..., ak = ak-1 + ak-2)
 'use strict';
+let fibonacci = function (num) {
+  if (num < 2) {
+    return num;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+};
+
+fibonacci(10);
