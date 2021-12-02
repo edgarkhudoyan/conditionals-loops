ea081d6f1e25f1d92ccb741da7c3732a4fea229c
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 14:08:28 +0400

    Solved using arrow function

--- task5.js
+++ task5.js
@@ -1,5 +1,10 @@
 // Given three numbers. Sort them by the ascending order.
 'use strict';
 
-let arr1 = [45, 26, 78];
+let arr1 = [45, 126, 78];
 let arr2 = [-23, -456, 0];
+
+let sorted = arr1.sort((a, b) => {
+  return a - b;
+});
+console.log(sorted);
