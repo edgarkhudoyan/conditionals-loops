42f56e188349070c037f06ed5558495a78afd714
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 23:52:27 +0400

    Separated numbers and strings

--- task_14.js
+++ task_14.js
@@ -1,1 +1,13 @@
 // Given an array of strings and numbers. Print the number of integers and the number of strings in the array.
+'use strict';
+
+let arr = [1, '10', 'hi', 2, 3];
+
+let nums = arr.filter((item) => {
+  return typeof item === 'number';
+});
+
+let strings = arr.filter((item) => {
+  return typeof item === 'string';
+});
+console.log(strings);
