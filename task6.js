13253e8a30120b946f7f6021678e657506fefff6
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 14:28:58 +0400

    used if conditional

--- task6.js
+++ task6.js
@@ -1,8 +1,11 @@
 // Find the sign of product of three numbers without multiplication operator. Display the specified sign.
 'use strict';
-let products = [0, 5, 3];
+let products = [-4, 5, 3];
 
 let sum = products.reduce((cur, acc) => {
   return cur * acc;
 });
-console.log(sum);
+
+if (sum === 0) console.log('unsigned');
+else if (sum > 0) console.log('+');
+else console.log('-');
