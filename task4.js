00c5fdefab5d936d2a39a7d9485d164be8030489
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 13:45:46 +0400

    Tried logical AND operator

--- task4.js
+++ task4.js
@@ -1,1 +1,7 @@
 // Check if a number is a multiple of 3, 5 or 7 and output the appropriate message.
+'use strict';
+let enterNumber = parseInt(prompt('Please enter a number'));
+
+if (enterNumber % 7 == 0 && enterNumber % 5 == 0) {
+  console.log(`${enterNumber} is a multiple of 7 and 5`);
+}
