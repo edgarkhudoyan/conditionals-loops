7faf5706354b8705260a3f841a6c6a13b7c77a6f
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 14:26:50 +0400

    Added reduce method

--- task6.js
+++ task6.js
@@ -1,3 +1,8 @@
 // Find the sign of product of three numbers without multiplication operator. Display the specified sign.
 'use strict';
-let product = [-14, 5, 0];
+let products = [0, 5, 3];
+
+let sum = products.reduce((cur, acc) => {
+  return cur * acc;
+});
+console.log(sum);
