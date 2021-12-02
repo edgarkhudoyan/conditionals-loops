068b953f169dcca8426325dd5ac42dd48eb9bf52
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 23:48:22 +0400

    Added filtered array

--- task_13.js
+++ task_13.js
@@ -5,3 +5,9 @@ let mixedArr = [8, 0, 1, 'hey', 12, 5, true, '2', null, 7, 3];
 let nums = mixedArr.filter((item) => {
   return typeof item === 'number';
 });
+
+let filtered = nums.sort(function (a, b) {
+  return (b % 2) - (a % 2) || a - b;
+});
+
+console.log(filtered);
