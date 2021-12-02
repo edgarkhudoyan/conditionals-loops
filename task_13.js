77f5155f3e39d4736b00cdd29400d24940a636f0
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 23:42:43 +0400

    Changed filtered array name

--- task_13.js
+++ task_13.js
@@ -2,6 +2,6 @@
 'use strict';
 let mixedArr = [8, 0, 1, 'hey', 12, 5, true, '2', null, 7, 3];
 
-let filteredArr = mixedArr.filter((item) => {
+let nums = mixedArr.filter((item) => {
   return typeof item === 'number';
 });
