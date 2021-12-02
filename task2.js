5feffb8aaacfddb6cd8cd7d9d6a8c223ecd61921
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 12:55:36 +0400

    Copied description and getting started

---
+++ task2.js
@@ -0,0 +1,6 @@
+// Given two variables, which are the angles of a triangle. Find the third angle of the triangle. (Sum of the angles of a triangle equals 180 degrees).
+'use strict';
+
+let angleOne = [45, 90];
+let angleTwo = [30, 30];
+let angleThree = [75.25, 80];
