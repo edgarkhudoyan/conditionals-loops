8bef7d0a434fcddca43cda7b0bf9cddb25b698dd
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-02 13:56:31 +0400

    Task 4 is done

--- task4.js
+++ task4.js
@@ -2,6 +2,18 @@
 'use strict';
 let enterNumber = parseInt(prompt('Please enter a number'));
 
-if (enterNumber % 7 == 0 && enterNumber % 5 == 0) {
+if (enterNumber % 3 != 0 && enterNumber % 5 != 0 && enterNumber % 7 != 0) {
+  alert(`${enterNumber} is not a multiple of 3, 5 or 7`);
+} else if (
+  enterNumber % 3 == 0 &&
+  enterNumber % 5 == 0 &&
+  enterNumber % 7 == 0
+) {
+  alert(`${enterNumber} is a multiple of 3, 5 and 7`);
+} else if (enterNumber % 7 == 0 && enterNumber % 5 == 0) {
   alert(`${enterNumber} is a multiple of 7 and 5`);
+} else if (enterNumber % 3 == 0 && enterNumber % 7 == 0) {
+  alert(`${enterNumber} is a multiple of 3 and 7`);
+} else if (enterNumber % 3 == 0) {
+  alert(`${enterNumber} is a multiple of 3`);
 }
