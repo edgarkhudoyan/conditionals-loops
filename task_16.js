a64db56c0d99a912831d1c732bc5becedb0edb3c
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-03 13:03:09 +0400

    Solved first part

--- task_16.js
+++ task_16.js
@@ -1,2 +1,10 @@
 // Given an array of numbers and a number. Find the index of a first element which is equal to that number. If there is not such a number, that find the index of the first element which is the closest to it.
-'use strict';
+let list = [21, -9, 15, 2116, -71, 33];
+let num = 15;
+
+for (let e of list) {
+  let index = list.indexOf(num);
+  if (num === e) {
+    console.log(index);
+  }
+}
