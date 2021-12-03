dc1b52936a4b096cb4ac7dda8835056f38cdbf4c
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-03 13:54:37 +0400

    Solved problem with if statement

--- task_17.js
+++ task_17.js
@@ -1,5 +1,12 @@
 // Given a sentence as a string. Split it according to space and comma and create an array consisting of the words of the array. The last word should not contain the last . or ! .
-let str = 'May the Force be with you.';
+let str = 'May the Force be with you';
 
-let toArr = str.split(' ');
-console.log(toArr);
+if (str[str.length - 1] === '.' || str[str.length - 1] === '!') {
+  let modified = str.slice(0, -1);
+
+  let toArr = modified.split(' ');
+
+  console.log(toArr);
+} else {
+  console.log(str.split(' '));
+}
