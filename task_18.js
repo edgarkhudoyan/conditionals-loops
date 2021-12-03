9de124246ac5cf894575aa1205d889771c99dcdd
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-03 14:40:55 +0400

    Deleted task_18 content

---
+++ task_18.js
@@ -0,0 +1,2 @@
+// Given an array of a size smaller than 100. It consists of numbers from 0 to 99 in any order. Create a new array where each element from that array is placed under the index of its value. Start from the smallest value and end with the biggest one. If there are missing values, put in its places undefined.
+let arr = [4, 3, 0, 9];
