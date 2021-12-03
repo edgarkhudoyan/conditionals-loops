8f1b9d8f5731cb7afc381f663ae78884ae6e2584
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-03 12:14:50 +0400

    Added task_16 description

---
+++ task_16.js
@@ -0,0 +1,2 @@
+// Given an array of numbers and a number. Find the index of a first element which is equal to that number. If there is not such a number, that find the index of the first element which is the closest to it.
+'use strict';
