21405f859a3c79cea0374098ffc3933b7a9df7c8
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-03 12:01:13 +0400

    Added Math.max and Math.min methods

--- task_15.js
+++ task_15.js
@@ -1,2 +1,11 @@
 // Given an array of strings. Find the strings with maximum and minimum lengths in array. Print the sum of their lengths.
 'use strict';
+let arr = ['anymore', 'raven', 'me', 'communicate'];
+let findLenght = arr.map((i) => {
+  return i.length;
+});
+
+let maximum = Math.max(...findLenght);
+let minimum = Math.min(...findLenght);
+
+// console.log(minimum);
